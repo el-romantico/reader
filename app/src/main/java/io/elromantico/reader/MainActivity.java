e4d051baa6137a9bdeb818c21b725d4d349c175f
Stanislav Gatev <mail@s2gatev.com>
2016-11-20 00:53:25 +0200

    Implement text-to-speech using NeoSpeech

--- app/src/main/java/io/elromantico/reader/MainActivity.java
+++ app/src/main/java/io/elromantico/reader/MainActivity.java
@@ -1,13 +1,28 @@
 package io.elromantico.reader;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.entity.StringEntity;
+
 public class MainActivity extends AppCompatActivity {
+    private SpeechSynthesizer speech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        am.setStreamVolume(AudioManager.STREAM_MUSIC, am.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+
+        speech = new SpeechSynthesizer(this);
+        speech.pronounce("This is a really cool app, guys!");
     }
 }
